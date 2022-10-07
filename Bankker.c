3e59e6aeb6e899a7d9dfaf6b95ca94c69437b4de
Parthasarathy <81454132+Parthasarathy-B@users.noreply.github.com>
2022-10-07 09:48:38 +0530

    Update Bankker.c

--- Bankker.c
+++ Bankker.c
@@ -1,10 +1,9 @@
-
 #include<stdio.h>
 #include<conio.h>
 #define max 25
 void main()
 {
-int frag[max],b[max],f[max],i,j,nb,nf,temp;
+int frag[max],b[max],f[max],i,j,nb,nf,temp,lowest=10000;
 static int bf[max],ff[max];
 clrscr();
 printf("\nEnter the number of blocks:");
@@ -31,18 +30,19 @@ if(bf[j]!=1)
 {
 temp=b[j]-f[i];
 if(temp>=0)
+if(lowest>temp)
 {
 ff[i]=j;
-break;
+lowest=temp;
 }
 }
 }
-frag[i]=temp;
+frag[i]=lowest;
 bf[ff[i]]=1;
+lowest=10000;
 }
-printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragment");
-for(i=1;i<=nf;i++)
+printf("\nFile_no  \tFile_size  \tBlock_no  \tBlock_size  \tFragment");
+for(i=1;i<=nf && ff[i]!=0;i++)
 printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
 getch();
 }
-
